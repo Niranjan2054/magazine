5899f55ff4c3bfae5aa9aff18f9f9cd928885826
Niranjan2054 <ckim6189@gmail.com>
2020-05-22 17:49:57 +0545

    Database module for Project

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php  
 	include $_SERVER['DOCUMENT_ROOT'].'config/init.php';
-	redirect('cms/index');
+	// redirect('cms/index');
+
+	$user = new user();
+	$data = array(
+		'username' => 'Khwopa',
+		'session_token'=>tokenize()
+	);
+	$user->deleteUserByEmail('khwopa@magazine.com');
+	
