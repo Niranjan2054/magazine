43b10c90dd77b361605776ea21a2f8b6c56b72aa
Niranjan2054 <ckim6189@gmail.com>
2020-05-27 21:31:27 +0545

    CMS: Category

--- class/category.php
+++ class/category.php
@@ -11,10 +11,20 @@
 
 		public function getCategorybyId($category_id,$is_die=false){
 			$args = array(
-				'fields' => "categoryname, email,password",
 				'where' => array(
 						'or' => array(
 							'id' => $category_id,
+						)
+					)
+			);
+			return $this->getData($args,$is_die);
+		}
+
+		public function getAllCategory($is_die=false){
+			$args = array(
+				'where' => array(
+						'or' => array(
+							'status'=>'Active',
 						)
 					)
 			);
